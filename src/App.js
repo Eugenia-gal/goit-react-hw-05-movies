7d116ae15aaa946b64240479ce7bea02f8d4128e
Eugenia-gal <galevbor12@gmail.com>
2021-08-04 17:44:47 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import { Route } from 'react-router-dom';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import Container from 'Components/Container';
 import AppBar from 'Components/AppBar/AppBar';
 import HomePage from 'Components/pages/HomePage.jsx';
 import MoviesPage from 'Components/pages/MoviesPage.jsx';
+import MovieDetailsPage from 'Components/pages/MovieDetailsPage';
+import NotFoundPage from 'Components/pages/NotFoundPage';
+import './App.css';
 
 function App() {
   return (
     <Container>
       <AppBar />
 
-      <Route path="/" exact>
-        <HomePage />
-      </Route>
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
 
-      <Route path="/movies">
-        <MoviesPage />
-      </Route>
+        <Route path="/movies" exact>
+          <MoviesPage />
+        </Route>
+
+        <Route path={`/movies/:movieId`}>
+          <MovieDetailsPage />
+        </Route>
+
+        <Route>
+          <NotFoundPage />
+        </Route>
+      </Switch>
     </Container>
   );
 }
